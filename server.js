773199704bcb502d70e9509b2e1caf7d0e6b84d4
Kristin Barr <kristinbarr@gmail.com>
2020-09-01 00:03:39 -0400

    added index file

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require("express");
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+function logger(req, res, next) {
+  console.log(
+    `Request method: ${req.method}, request URL: ${
+      req.url
+    }, timestamp: ${new Date()}`
+  );
+  next();
+}
+
+server.use(logger);
+server.use(express.json());
+
+server.use("/api/users", userRouter);
+
+// test GET route
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
+// test POST route
+server.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send("working route");
+});
 
 module.exports = server;
